62e9b636e266110e97a8d13e5a2a0d9d0ed908af
Gabriele <41295383+gabrielezi@users.noreply.github.com>
2019-12-20 16:06:23 +0200

    html

---
+++ 04 Laboratorinis/iOS-BookFlight-App/resources/masters/ead4920d-bc72-485d-9d8a-d7fd3fe9c9d6-1576850610435.js
@@ -0,0 +1,54 @@
+jQuery("#simulation")
+  .on("click", ".m-ead4920d-bc72-485d-9d8a-d7fd3fe9c9d6 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#m-ead4920d-Hotspot_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/e39b167a-6827-498b-8062-bf89d74c1e48"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#m-ead4920d-Hotspot_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/f25bbb63-30e3-4c35-a767-bc1bfa17c128"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
